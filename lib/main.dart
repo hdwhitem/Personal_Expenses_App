b8cff4890b1978317c2cbdc17689070e486827df
hdwhitem <hdwhite.m@gmail.com>
2022-04-18 09:54:03 +0100

    Adding Images to the App

--- lib/main.dart
+++ lib/main.dart
@@ -37,42 +37,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Book newBk;
-  final List<Book> _userBooks = [
-    Book(
-      id: 'p1',
-      title: 'Beginning Flutter With Dart',
-      description: 'You can learn Flutter as well Dart.',
-      price: 9.99,
-      date: DateFormat.yMMMEd().format(
-        DateTime.now(),
-      ),
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg',
-    ),
-    Book(
-      id: 'p2',
-      title: 'Flutter State Management',
-      description: 'Everything you should know about Flutter State.',
-      price: 9.99,
-      date: DateFormat.yMMMEd().format(
-        DateTime.now(),
-      ),
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_960_720.jpg',
-    ),
-    Book(
-      id: 'p3',
-      title: 'WordPress Coding',
-      description:
-          'WordPress coding is not difficult, in fact it is interesting.',
-      price: 9.99,
-      date: DateFormat.yMMMEd().format(
-        DateTime.now(),
-      ),
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010_960_720.jpg',
-    ),
-  ];
+  final List<Book> _userBooks = [];
 
   void _addNewBook(String txTitle, double txPrice) {
     setState(() {
--- lib/widgets/book_list.dart
+++ lib/widgets/book_list.dart
@@ -11,50 +11,60 @@ class BookList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 550,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-              child: Row(
-            children: <Widget>[
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                  width: 2,
-                )),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  '\$${books[index].price.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    books[index].title,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Text(
-                    books[index].date,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
+      child: books.isEmpty
+          ? Column(
+              children: <Widget>[
+                const Text('No Books added yet'),
+                Image.asset(
+                  'assets/zzz.png',
+                  fit: BoxFit.cover,
+                )
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        '\$${books[index].price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              )
-            ],
-          ));
-        },
-        itemCount: books.length,
-      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          books[index].title,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          books[index].date,
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ));
+              },
+              itemCount: books.length,
+            ),
     );
   }
 }
